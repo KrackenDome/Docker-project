0d0c04d1788523dfff03740a5fa2f0609e04c7ed
KrackenDome <112581959+KrackenDome@users.noreply.github.com>
2023-06-05 15:32:42 +0200

    Initial commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,64 @@
 @extends('layout')
 
 @section('content')
-    <div class="container">
-        <div class="mb-3">
-            <a href="{{ route('students.create') }}" class="btn btn-success">Create</a>
-        </div>
+
 <style>
   .push-top {
     margin-top: 50px;
   }
-</style>
+            .navbar {
+                background-color: rgba(255, 255, 255, 0.7);
+            }
 
+            .navbar .nav-link {
+                color: #333;
+            }
+
+            .nav-link{
+                color: #000;
+                margin: 0 10px;
+                text-decoration: none;
+            }
+
+            .navbar .nav-link:hover {
+                background-color: lightgray;
+            }
+
+            .has-text-centered {
+                text-align: center;
+            }
+        </style>
+
+        <nav class="navbar py-0 navbar-expand-lg navbar-light navbar-laravel" role="navigation" aria-label="main navigation">
+            <div class="container div2" id="navbarBasicExample navbar-menu">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    @guest
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">Register</a>
+                            </li>
+                        </ul>
+                    @else
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+                            </li>
+                        </ul>
+                    @endguest
+
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="mb-3">
+                <a href="{{ route('students.create') }}" class="btn btn-success">Create</a>
+            </div>
 <div class="push-top">
-  @if(session()->get('success'))
-    <div class="alert alert-success">
-      {{ session()->get('success') }}
-    </div><br />
-  @endif
   <table class="table">
     <thead>
         <tr class="table-warning">
